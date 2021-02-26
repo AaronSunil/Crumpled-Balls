f226c7338ca73e373301739e261f7dc8deb96092
AaronSunil <72694245+AaronSunil@users.noreply.github.com>
2021-02-26 11:46:45 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -3,7 +3,7 @@ class Paper {
       var options = {
           isStatic:false,
           'restitution':0.3,
-          'friction':0.8,
+          'friction':0.5,
           'density':1.2
       }
       this.body = Bodies.circle(x, y, 1,options);
